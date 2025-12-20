0170945f099eb4814dba5e06c0e3b85cfd93bb63
AmmarBinYasir09 <Ammarbinyasir2007@gmail.com>
2025-12-20 15:44:25 +0500

    Added the Student and Course structs

--- PF-theory-project/main.cpp
+++ PF-theory-project/main.cpp
@@ -1,8 +1,45 @@
 #include <iostream>
+#include <string>
 using namespace std;
+
+// Constants
+int TotalSubjects = 7;
+
+
+
+struct Course
+{
+	string courseName;
+	int crHrs;
+
+	float quiz[2], assignment, mids, finals;
+
+	// total wieghted marks out of 100
+	float totalMarks;
+
+	char grade[2]
+};
+
+struct Student
+{
+	int id;
+	string name;
+
+	Course subjects[TotalSubjects];
+
+	// wieghted sum of all course points / total cr.hrs
+	float sgpa;
+
+	char finalGrade[2];
+};
+
+
+
+
 
 int main()
 {
-	cout << "this is a change";
+
+
 	return 0;
 }
