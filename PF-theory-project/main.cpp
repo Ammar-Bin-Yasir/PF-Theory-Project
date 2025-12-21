b26dcae71dee710460d24532b4095ebbe2cc5327
AmmarBinYasir09 <Ammarbinyasir2007@gmail.com>
2025-12-21 15:53:31 +0500

    Layed out the structure of the main function

--- PF-theory-project/main.cpp
+++ PF-theory-project/main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 // Constants
 const int TotalSubjects = 7;
-
+const int TotalStudents = 50;
 
 
 struct Course
@@ -34,12 +34,95 @@ struct Student
 };
 
 
-
+void loadRecords(Student student[]);
 
 
 int main()
 {
+    // Allocated dynamic memory because stack memory was getting overload
+    Student* students = new Student[TotalStudents]{};
 
+    // Load the data from files into the structures
+    loadRecords(students);
+	
+    cout << "=====================================================\n" 
+		 << "||            Student Management System            ||\n"
+		 << "=====================================================\n\n";
 
-	return 0;
+	int n;
+	do
+	{
+		cout << "1. Display the table of all (current) records.\n"
+			 << "2. Search a Student based on their ID.\n"
+			 << "3. Update Student Marks.\n"
+			 << "4. Delete Student from the table.\n"
+			 << "5. Sort the Student list in (ACS/DESC).\n"
+			 << "6. View Analytics & Statistics.\n"
+			 << "7. Export Reports in a file.\n"
+			 << "8. Save and Exit.\n";
+
+		cin >> n;
+
+		switch (n)
+		{
+			// Display the struct in a ascii art type table
+			case 1:
+			{
+				break;
+			}
+
+			// Search and display the student based on it's ID
+			case 2:
+			{
+				break;
+			}
+
+			// Update the Student data in the struct table
+			case 3:
+			{
+				break;
+			}
+
+			// Delete the student records from the table
+			case 4:
+			{
+				break;
+			}
+
+			// Sort and display the table in Ascending or Descending order
+			case 5:
+			{
+				break;
+			}
+
+			// provide options to see analytical reports (topper list, course-wise averages, student paas fail ratio)
+			case 6:
+			{
+				break;
+			}
+
+			// Export the reports in a text file
+			case 7:
+			{
+				break;
+			}
+
+			// Save the existing struct in the student.csv file and Exit
+			case 8:
+			{
+				break;
+			}
+		}
+		
+
+	} while (n != 8);
+    
+    delete[] students;
+
+    return 0;
+}
+
+void loadRecords(Student student[])
+{
+
 }
