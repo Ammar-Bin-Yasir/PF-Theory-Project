173c2054d58dc94bc7de8cadd163285ff5108f23
AmmarBinYasir09 <Ammarbinyasir2007@gmail.com>
2025-12-24 12:25:17 +0500

    made a mockdata function for testing

--- PF-theory-project/main.cpp
+++ PF-theory-project/main.cpp
@@ -122,7 +122,35 @@ int main()
     return 0;
 }
 
-void loadRecords(Student student[])
+
+
+
+
+
+void loadRecords(Student students[])
 {
+
+}
+
+
+void rankStudents(Student s[])
+{
+
+}
+
+void loadMockData(Student students[]) {
+	students[0].id = 2025560;
+	students[0].name = "Ammar Bin Yasir";
+
+	// Fill one subject for testing
+	students[0].subjects[0].courseName = "PF";
+	students[0].subjects[0].crHrs = 3;
+	students[0].subjects[0].quiz[0] = 8;     // out of 10
+	students[0].subjects[0].quiz[1] = 9;
+	students[0].subjects[0].assignment = 10;
+	students[0].subjects[0].mids = 25;       // out of 30
+	students[0].subjects[0].finals = 40;     // out of 50
+
+	// You would call calculateGPA() here next!
 
 }
